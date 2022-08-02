378f512ecdf7f3eabf044720a7fbe57d53439b81
Your Name <pjduri@gmail.com>
2022-08-01 23:36:47 -0500

    got booleans into validateInput.  current error in console: scriptHelper.js:46 Uncaught ReferenceError: Cannot access 'validateInput' before initialization

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-// Write your JavaScript code here!
+//const { formSubmission } = require("./scriptHelper");
 
+// Write your JavaScript code here!
 window.addEventListener("load", function() {
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+    // these querySelectors work
+    const testForm = this.document.querySelector("form")
+    const formField = this.document.querySelector("formField")
+    const formSubmit = this.document.getElementById('formSubmit')
+    const pilotName = this.document.querySelector('input[name=pilotName]')
+    const faultyItems = this.document.querySelector('input[name=faultyItems]')
+    const copilotName = this.document.querySelector('input[name=copilotName]')
+    const fuelLevel = this.document.querySelector('input[name=fuelLevel]')
+    const cargoMass = this.document.querySelector('input[name=cargoMass]')
+ 
+    // re-read 23.5
+    // get button
+    // addEventListener on button
+    // add form validation inside button event listener
+    console.log('pilot name', pilotName)
+    console.log("testForm", testForm)
+    console.log('formSubmit ', formSubmit)
+    //console.log(formSubmit)
+    formSubmit.addEventListener("click", function (event) {
+        event.preventDefault()
+        console.log(formSubmission(pilotName, copilotName, fuelLevel, cargoMass))
+    })
+
+//    let listedPlanets;
+//    // Set listedPlanetsResponse equal to the value returned by calling 
+//    // myFetch()
+//    let listedPlanetsResponse;
+//    listedPlanetsResponse.then(function (result) {
+//        listedPlanets = result;
+//        console.log(listedPlanets);
+//    }).then(function () {
+//        console.log(listedPlanets);
+//        // Below this comment call the appropriate helper functions to 
+//        // pick a planet fom the list of planets and add that information 
+//        // to your destination.
+//    })
    
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,7 +1,17 @@
 // Write your helper functions here!
 require('isomorphic-fetch');
 
-function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
+// const form = this.document.getElementById('testForm')
+// const button = this.document.getElementById('formSubmit')
+// const messages = []
+// const pilotName = this.document.getElementById('pilotName')
+// const copilotName = this.document.getElementById('copilotName')
+// const fuelLevel = this.document.getElementById('fuelLevel')
+// const cargoMass = this.document.getElementById('cargoMass')
+// const faultyItems = this.document.getElementById('faultyItems')
+
+function addDestinationInfo(document, name, diameter, star, 
+    distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
    /*
                 <h2>Mission Destination</h2>
@@ -16,12 +26,27 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
    */
 }
 
-function validateInput(testInput) {
-   
+const validateInput = (testInput) => {
+    const inputReturns = []
+    if(testInput === '' || testInput == null) {
+        inputReturns.push('Empty')
+    }
+    if(isNaN(testInput)) {
+        inputReturns.push('Not a number')
+    } else if(!isNaN(testInput)) {
+        inputReturns.push('Is a number')
+    }
+    return inputReturns
 }
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+// function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+function formSubmission(pilot, copilot, fuelLevel, cargoLevel) {
+   const testArr = [pilot, copilot, fuelLevel, cargoLevel]
+   for (i=0; i<testArr.length; i++) {
+    if (validateInput(testArr[i]) === 'Empty') {
+        alert('All fields required')
+    }
+   }
 }
 
 async function myFetch() {
@@ -35,6 +60,14 @@ async function myFetch() {
 
 function pickPlanet(planets) {
 }
+
+// module.exports = {
+//     addDestinationInfo: addDestinationInfo,
+//     validateInput: validateInput,
+//     formSubmission: formSubmission,
+//     pickPlanet: pickPlanet,
+//     myFetch: myFetch
+// }
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
